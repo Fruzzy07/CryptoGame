48a485827f82afadfcabb3221e70e579f32a5a6d
Fruzzy07 <shapkaternur@gmail.com>
2024-12-26 18:10:20 +0500

    Updated Miner.js

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,58 +1,58 @@
-import './Square.css';
-import hoverEffect from '../assets/Sound/hover.wav';
-import DiamondEffect from '../assets/Sound/gold.wav';
-import goldIcon from '../assets/gold.png';
-import bombIcon from '../assets/bomb.png';
-import { useEffect, useState } from 'react';
-
-function Square({ mine, setGameOver, gameOver, setScore }) {
-
-    let [image, setImage] = useState(null);
-
-    useEffect(() => {
-        if (gameOver) {
-            if (mine) {
-                setImage(bombIcon);
-            }
-            else {
-                setImage(goldIcon);
-            }
-        }
-    }, [gameOver, mine])
-
-    function mouseEnterHandle() {
-        if (!image) {
-            const sound = new Audio(hoverEffect);
-            sound.play();
-        }
-    }
-
-    function clickHandler() {
-
-        if(gameOver) return;
-
-        if (!mine) {
-            setScore((prevValue) => {
-                return prevValue * 2;
-            });
-            setImage(goldIcon);
-            const sound = new Audio(DiamondEffect);
-            sound.play();
-        } else {
-            alert("You Loose The Game");
-            setGameOver(true);
-        }
-    }
-
-    return <>
-        <div
-            className='square-item'
-            onMouseEnter={mouseEnterHandle}
-            onClick={clickHandler}
-        >
-            {image && <img height={90} width={90} src={image} />}
-        </div>
-    </>
-}
-
+import './Square.css'; 
+import hoverEffect from '../assets/Sound/hover.wav'; 
+import DiamondEffect from '../assets/Sound/gold.wav'; 
+import goldIcon from '../assets/gold.png'; 
+import bombIcon from '../assets/bomb.png'; 
+import { useEffect, useState } from 'react'; 
+ 
+function Square({ mine, setGameOver, gameOver, setScore }) { 
+    let [clicked, setClicked] = useState(false); // Track if this square is clicked 
+    let [image, setImage] = useState(null); 
+ 
+    useEffect(() => { 
+        if (gameOver) { 
+            // Reveal all squares when the game is over 
+            if (mine) { 
+                setImage(bombIcon); // Show bomb for mines 
+            } else { 
+                setImage(goldIcon); // Show gold for non-mines 
+            } 
+        } 
+    }, [gameOver, mine]); 
+ 
+    function mouseEnterHandle() { 
+        if (!image && !clicked) { 
+            const sound = new Audio(hoverEffect); 
+            sound.play(); 
+        } 
+    } 
+ 
+    function clickHandler() { 
+        if (gameOver || clicked) return; // Prevent further actions if already clicked or game is over 
+ 
+        setClicked(true); // Mark the square as clicked 
+ 
+        if (!mine) { 
+            setScore((prevValue) => prevValue * 2); // Update score 
+            setImage(goldIcon); // Show gold icon 
+            const sound = new Audio(DiamondEffect); 
+            sound.play(); 
+        } else { 
+            alert("You Lose The Game"); 
+            setGameOver(true); // End the game 
+            setImage(bombIcon); // Show bomb icon 
+        } 
+    } 
+ 
+    return ( 
+        <div 
+            className='square-item' 
+            onMouseEnter={mouseEnterHandle} 
+            onClick={clickHandler} 
+        > 
+            {image && <img height={90} width={90} src={image} />} 
+        </div> 
+    ); 
+} 
+ 
 export default Square;
